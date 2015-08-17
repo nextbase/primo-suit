e5e3277c94e90edc282381253a68bdf59399844b
Igor Putina <igor@next-aeon.com>
2015-08-17 09:51:21 +0900

    Deploying changes

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-	$("#sitemap-button").click(function() {
-		$("#footer").toggleClass("sitemap-expanded");
-	});
+	var sitemap_toggle = $('#sitemap-toggle');
+    var sitemap_block  = $('#footer-sitemap');
+
+    sitemap_toggle.click(function(e) {
+        e.preventDefault();
+        sitemap_block.toggleClass('expanded');
+    });
 });
