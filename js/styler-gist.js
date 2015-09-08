239dd473ef4a6891d5dbecad5299f4c1e0990291
Igor Putina <igor@next-aeon.com>
2015-09-08 14:20:08 +0900

    Deploying changes

--- js/styler-gist.js
+++ js/styler-gist.js
@@ -4,3 +4,16 @@ setTimeout(function() {
     example.src = "http://nextbase.github.io/primo-suit/js/example.js";
     document.body.appendChild(example);
 }, 4000);
+
+removeAllStylesheetLinks();
+
+function removeAllStylesheetLinks() {
+    var stylesheets = document.getElementsByTagName('link'), i, sheet;
+
+    for(i in stylesheets) {
+        sheet = stylesheets[i];
+
+        if(sheet.getAttribute('type').toLowerCase() == 'text/css')
+            sheet.parentNode.removeChild(sheet);
+    }
+}
