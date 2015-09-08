c1ce83460c5d62834bb1e3d8a5cd6d2e3aeb9eca
Igor Putina <igor@next-aeon.com>
2015-09-08 12:28:24 +0900

    Added a small method for adding JS using Styler Plugin for Chrome

---
+++ js/styler-gist.js
@@ -0,0 +1,6 @@
+// I used this method to add a JS using Styler Plugin for Chrome
+setTimeout(function() {
+    var example = document.createElement('script');
+    example.src = "http://nextbase.github.io/primo-suit/js/example.js";
+    document.body.appendChild(example);
+}, 4000);
